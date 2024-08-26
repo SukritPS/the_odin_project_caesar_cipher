e6f26079fc0627b618cf5b0d93e2bdeadf8ceb1b
Sukrit Phongcharatkawin <sukritps.16@gmail.com>
2024-08-26 08:27:12 +0700

    Init

---
+++ caesar_cipher.rb
@@ -0,0 +1,17 @@
+def caesar_cipher(string, shift=5)
+  string.chars.map do |char|
+    if char.match?(/[a-zA-z]/)
+      shift.times { char = char.next }
+      char[-1]
+    else
+      char
+    end
+  end.join
+end
+
+print "Enter a shifted value (default: 5): "
+shift = gets.chomp.to_i
+print "Enter a string to encrypt: "
+plaintext = gets.chomp
+encrypted = caesar_cipher(plaintext, shift)
+puts "Encrypted string: #{encrypted}"
